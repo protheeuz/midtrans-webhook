76774960b40e8e9b72104ab2c05dd175f4de340a
Iqbal Fauzi <iqbalfauzien@gmail.com>
2024-07-08 04:25:41 +0700

    mongoose

--- mongodb.js
+++ mongodb.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const uri = process.env.MONGODB_URI;
+console.log('Connecting to MongoDB with URI:', uri); // Tambahkan logging
 
 const connectToDatabase = async () => {
     try {
--- server.js
+++ server.js
@@ -18,6 +18,7 @@ const MIDTRANS_CLIENT_KEY = process.env.MIDTRANS_CLIENT_KEY;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views'); // Pastikan path ini benar
 
 connectToDatabase();
 
