4623f317eba062ca29fa0707412e065e420cfbf0
Iqbal Fauzi <iqbalfauzien@gmail.com>
2024-07-08 17:50:25 +0700

    mongoose timeout

--- mongodb.js
+++ mongodb.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose');
-require('dotenv').config();
 
 const connectToDatabase = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI);
+        await mongoose.connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 5000 // Time out after 5s instead of 30s
+        });
         console.log('Connected to MongoDB');
     } catch (error) {
         console.error('MongoDB connection error:', error);
